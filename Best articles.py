62cb8886ad5a9675d07847477ddc5d2ebfa75403
KowalewskiMateusz <39586192+KowalewskiMateusz@users.noreply.github.com>
2019-05-03 21:56:32 +0200

    Add files via upload

--- Best articles.py
+++ Best articles.py
@@ -1,26 +1,39 @@
 import requests
 from operator import itemgetter
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
-print("Status code:", r.status_code)
 submission_ids = r.json()
 submission_dicts = []
+names = []
 for submission_id in submission_ids[:30]:
     url = ('https://hacker-news.firebaseio.com/v0/item/' +
            str(submission_id) + '.json')
     submission_r = requests.get(url)
-    print(submission_r.status_code)
     response_dict = submission_r.json()
-
     submission_dict = {'title': response_dict['title'],
-                       'link': 'http://news.ycombinator.com/item?id=' + str(submission_id),
-                       'comments': response_dict.get('descendants', 0)}
+                       'xlink': response_dict['url'],
+                       'value': response_dict.get('descendants', 0)}
+    names.append(submission_dict['title'])
     submission_dicts.append(submission_dict)
 
-submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+submission_dicts = sorted(submission_dicts, key=itemgetter('value'), reverse=True)
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
 
-for submission_dict in submission_dicts:
-    print("\nTitle:", submission_dict['title'])
-    print("Discussion link:", submission_dict['link'])
-    print("Comments:", submission_dict['comments'])
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Hottest discussion'
+chart.x_labels = names
+chart.add('', submission_dicts)
+chart.render_to_file('Hottest discussion.svg')
